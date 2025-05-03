b2acafecda1c6c6b76839666ea9d2b38dd8fa4b7
Amnamot <amnamot@email.com>
2025-05-03 15:27:39 +0300

    консоль.лог инитдата

--- src/utils/init-data.service.ts
+++ src/utils/init-data.service.ts
@@ -4,6 +4,7 @@ import * as crypto from 'crypto';
 @Injectable()
 export class InitDataService {
   validateInitData(initData: string, botToken: string): boolean {
+    console.log('🧪 BOT_TOKEN (from env):', botToken);
     const parsed = new URLSearchParams(initData);
 
     parsed.delete('signature'); // ✅ удаляем перед проверкой
