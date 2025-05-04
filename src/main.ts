55ee1948879448cb5ccb476332d963d72e3c8203
Amnamot <amnamot@email.com>
2025-05-04 10:07:07 +0300

    чиним initdata

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-// main.ts
+// src/main.ts 
+// v1
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -7,13 +8,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://igra.top'], // ✅ разрешаем фронт
+    origin: ['https://igra.top'], // ✅ разрешённый фронтовый домен
     credentials: true,
     methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'x-init-data'], // ✅ разрешаем заголовки
+    allowedHeaders: ['Content-Type', 'x-init-data'], // ✅ разрешённые заголовки
   });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
   await app.listen(3000);
 }
 bootstrap();
