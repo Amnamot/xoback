f399b312e057d0a140eddc2d2d6e65457c76ba13
Amnamot <amnamot@gmail.com>
2025-05-05 18:55:16 +0300

    temp

--- src/lobby/lobby.service.ts
+++ src/lobby/lobby.service.ts
@@ -1,10 +1,10 @@
 // src/lobby/lobby.service.ts v1
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InitDataParsed } from '../utils/init-data.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 import { Redis } from 'ioredis';
 import { InjectRedis } from '@nestjs-modules/ioredis';
-import { InitDataParsed } from '../utils/init-data.service';
 import axios from 'axios';
 
 @Injectable()
@@ -17,17 +17,42 @@ export class LobbyService {
 
   async createLobby(initData: InitDataParsed) {
     const telegramId = initData.user?.id?.toString();
-    if (!telegramId) throw new Error('Telegram ID not found in initData');
+    const firstName = initData.user?.first_name || 'Игрок';
 
-    const user = await this.prisma.user.findUnique({ where: { telegramId } });
-    if (!user) throw new Error('User not found');
+    if (!telegramId) {
+      throw new UnauthorizedException('Invalid Telegram ID');
+    }
+
+    const user = await this.prisma.user.findUnique({
+      where: { telegramId },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
 
     const lobbyId = `lobby_${Date.now()}_${Math.floor(Math.random() * 1000)}`;
-    await this.redis.set(lobbyId, telegramId, 'EX', 180); // 3 минуты
+    await this.redis.set(lobbyId, telegramId, 'EX', 180);
+
+    const inviteLink = `https://t.me/TacTicToe_bot?startapp=${lobbyId}`;
 
     const botToken = this.configService.get<string>('BOT_TOKEN');
-    const channelId = this.configService.get<string>('INVITE_CHANNEL_ID');
-    const inviteLink = `https://t.me/TacTicToe_bot?startapp=${lobbyId}`;
+    const channelId = this.configService.get<string>('INVITE_CHANNEL_ID') || '-1002654297071';
+    const imageUrl = 'https://igra.top/media/inviteImg.png';
+
+    await axios.post(`https://api.telegram.org/bot${botToken}/sendPhoto`, {
+      chat_id: channelId,
+      photo: imageUrl,
+      caption: `${firstName} вызывает тебя на поединок в TacTicToe!\nНажми кнопку ниже, чтобы принять вызов.`,
+      reply_markup: {
+        inline_keyboard: [[
+          {
+            text: 'Принять вызов',
+            url: inviteLink,
+          }
+        ]]
+      }
+    });
 
     return { lobbyId, inviteLink };
   }
