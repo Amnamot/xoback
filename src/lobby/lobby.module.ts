f024d928bade6962f736b781504ee00c73f4b56a
Amnamot <amnamot@gmail.com>
2025-05-06 16:06:11 +0300

    кнопка Start

--- src/lobby/lobby.module.ts
+++ src/lobby/lobby.module.ts
@@ -1,10 +1,12 @@
+// src/lobby/lobby.module.ts v1
 import { Module } from '@nestjs/common';
 import { LobbyController } from './lobby.controller';
 import { LobbyService } from './lobby.service';
 import { UtilsModule } from '../utils/utils.module';
+import { HttpModule } from '@nestjs/axios'; // ✅ добавлено
 
 @Module({
-  imports: [UtilsModule],
+  imports: [UtilsModule, HttpModule], // ✅ добавлено
   controllers: [LobbyController],
   providers: [LobbyService]
 })
